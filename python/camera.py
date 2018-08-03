d58858a0af2b458524fca0be75848c170bded1a2
DannyDannyDanny <s144222@student.dtu.dk>
2018-08-03 16:37:23 +0200

    removed redundant comments

--- python/camera.py
+++ python/camera.py
@@ -17,21 +17,7 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    # for (x,y,w,h) in faces:
-    #     print(x,y,w,h)
-    #     roi_gray = gray[y:y+h, x:x+w]
-    #     roi_color = frame[y:y+h, x:x+w]
-    #
-    #     color = (255,100,100) #BGR 0-255,
-    #     stroke = 3
-    #     cv2.rectangle(frame, (x,y),(x+w,y+h),color, stroke)
-    #
-    #     #img_item = "my-image.png"
-    #     #cv2.imwrite(img_item,roi_gray)
-
+    #---> barcode reading every frame
 
     # Display the resulting frame
     cv2.imshow('feed',cv2.flip(frame,1))
