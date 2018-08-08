e523083eaf4dcdd3be0fb84de84c1218bc0c74d4
DannyDannyDanny <s144222@student.dtu.dk>
2018-08-08 19:15:24 +0200

    live recognition - not renderingboxes

---
+++ python/camera_w_recognition.py
@@ -0,0 +1,57 @@
+import cv2
+import pyzbar.pyzbar as pyzbar
+from IPython.display import Image
+from pyzbar.pyzbar import ZBarSymbol
+from matplotlib import pyplot as plt
+from time import sleep
+
+
+print('loading camera stream')
+cap = cv2.VideoCapture(0)
+
+print('starting loop')
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('feed',cv2.flip(frame,1))
+    #cv2.imshow('gray',gray)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+while(True):
+    # Capture frame-by-frame
+    print('reading frame')
+    sleep(0.5)
+    ret, frame = cap.read()
+
+    barcodes = pyzbar.decode(frame, symbols=[ZBarSymbol.CODE128])
+
+    for barcode in barcodes:
+        (x,y,w,h) = barcode.rect
+
+        color = (255,100,100) #BGR 0-255,
+        stroke = 3
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 1
+        fontColor = (255,255,255)
+        lineType = 2
+
+        cv2.putText(frame, barcode.data, (x,y), font, fontScale,fontColor,lineType)
+        cv2.rectangle(frame, (x,y),(x+w,y+h),color, stroke)
+
+
+
+    # Display the resulting frame
+    cv2.imshow('feed',cv2.flip(frame,1))
+    cv2.imshow('feed',frame)
+    #cv2.imshow('gray',gray)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+print('shutting down')
+cap.release()
+cv2.destroyAllWindows()
+exit()
